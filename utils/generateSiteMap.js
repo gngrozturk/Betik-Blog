37190068ff3a252422b01b29592f419af23f6eb7
bufgix <ooruc471@yandex.com>
2020-11-21 18:13:41 +0300

    fix: remove intentation

--- utils/generateSiteMap.js
+++ utils/generateSiteMap.js
@@ -11,27 +11,27 @@ async function generateSiteMap() {
   ]);
 
   const sitemap = `
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-          ${pages
-            .map((page) => {
-              const path = page
-                .replace("pages", "")
-                .replace(".js", "")
-                .replace(".md", "")
-                .replace("out/", "")
-                .replace(".html", "")
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+      .map((page) => {
+        const path = page
+          .replace("pages", "")
+          .replace(".js", "")
+          .replace(".md", "")
+          .replace("out/", "")
+          .replace(".html", "")
 
-              const route = path === "/index" ? "" : path;
-              return `
-                      <url>
-                          <loc>${`https://www.betikblog.com/${route}`}</loc>
-                      </url>
-                  `;
-            })
-            .join("")}
-      </urlset>
-  `;
+        const route = path === "/index" ? "" : path;
+        return `
+                <url>
+                    <loc>${`https://www.betikblog.com/${route}`}</loc>
+                </url>
+            `;
+      })
+      .join("")}
+</urlset>
+`;
 
   fs.writeFileSync("public/sitemap.xml", sitemap);
 }
